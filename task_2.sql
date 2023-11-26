a778952380e745e8d8f9c7527c37f522b572e518
Nina Konopleva <orangemise@gmail.com>
2023-11-26 20:43:01 +0300

    add homework seminar 6

---
+++ task_2.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+CREATE PROCEDURE evenNumbers()
+BEGIN
+    DECLARE n INT default 0;
+    DROP TABLE IF EXISTS nums;
+    CREATE TABLE nums (n INT);
+
+    WHILE n < 10 DO
+    SET n = n + 2;
+    INSERT INTO nums VALUES(n);
+    END WHILE;
+
+SELECT * FROM nums;
+END $$
+DELIMITER ;
+
+CALL evenNumbers();
